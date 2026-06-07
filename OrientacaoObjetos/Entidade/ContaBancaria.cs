1e73bfea89fe5f232e08b85d5aeace52390fd998
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-06-06 21:09:20 -0300

    Add project files.

---
+++ OrientacaoObjetos/Entidade/ContaBancaria.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrientacaoObjetos.Entidade
+{
+    public class ContaBancaria
+    {
+        public string Titular;
+        public string Numero;
+        public string Agencia;
+        public double Saldo;
+
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+            Console.WriteLine($"Deposito de R$ {valor} realizado. Saldo atual: R$ {Saldo}");
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor <= Saldo)
+            {
+                Saldo -= valor;
+                Console.WriteLine($"Saque de R$ {valor} realizado. Saldo atual: R$ {Saldo}");
+
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente para realizar o saque.");
+            }
+        }
+
+        public void MostrarDados()
+        {
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine($"Titular: {Titular}");
+            Console.WriteLine($"Conta: {Numero}");
+            Console.WriteLine($"Agência: {Agencia}");
+            Console.WriteLine($"Saldo: R$ {Saldo}");
+            Console.WriteLine("---------------------------------------------");
+        }
+    }
+}
---
+++ OrientacaoObjetos/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using OrientacaoObjetos.Entidade;
+
+ContaBancaria conta = new ContaBancaria();
+
+conta.Titular = "Gustavo Gfregorio";
+conta.Numero = "123456";
+conta.Agencia = "0001";
+conta.Saldo = 1000;
+
+conta.Sacar(500);
+conta.Depositar(1500);
+conta.Sacar(10000);
+
+conta.MostrarDados();
